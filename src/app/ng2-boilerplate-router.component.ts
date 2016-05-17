6065218a55c78ff18d0f3473ccc4c1b28cff5aa7
angular-cli <angular-cli@angular.io>
2016-05-17 00:08:52 -0300

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ src/app/ng2-boilerplate-router.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'ng2-boilerplate-router-app',
+  templateUrl: 'ng2-boilerplate-router.component.html',
+  styleUrls: ['ng2-boilerplate-router.component.css']
+})
+export class Ng2BoilerplateRouterAppComponent {
+  title = 'ng2-boilerplate-router works!';
+}

5135905e93b147860100f1921b51fe73a77e246e
Jorge Cano <jorgeucano@MacBook-Pro-de-Jorge.local>
2016-05-17 00:43:36 -0300

    add routing and template routing

--- src/app/ng2-boilerplate-router.component.ts
+++ src/app/ng2-boilerplate-router.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterdashComponent } from './+routerdash';
+import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'ng2-boilerplate-router-app',
   templateUrl: 'ng2-boilerplate-router.component.html',
-  styleUrls: ['ng2-boilerplate-router.component.css']
+  styleUrls: ['ng2-boilerplate-router.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS]
 })
+@Routes([
+  {path: '/routerdash', component: RouterdashComponent}
+])
 export class Ng2BoilerplateRouterAppComponent {
   title = 'ng2-boilerplate-router works!';
 }
